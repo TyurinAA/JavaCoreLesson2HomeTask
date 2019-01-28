1540824ddb78d5522e725ded8bd8ba0e89273485
Andrey.Tyurin <aatyurin@outlook.com>
2019-01-28 21:04:52 +0300

    Added: 1. new exception invalid value conversion 2. getter, setter and constructor for exception

---
+++ src/MyArrayDataException.java
@@ -0,0 +1,16 @@
+public class MyArrayDataException extends Throwable {
+    private int rowIndex, columnIndex;
+
+    public MyArrayDataException(int rowIndex, int columnIndex) {
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public int getColumnIndex() {
+        return columnIndex;
+    }
+}
