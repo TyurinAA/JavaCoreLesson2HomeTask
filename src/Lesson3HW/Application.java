a387e2975b27273a12f1aa4c5264ee90105a1c21
Andrey.Tyurin <aatyurin@outlook.com>
2019-01-31 11:09:03 +0300

    new class added, remove duplicates done

--- src/Lesson3HW/Application.java
+++ src/Lesson3HW/Application.java
@@ -1,13 +1,28 @@
 package Lesson3HW;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> array = new ArrayList<>();
+        List<String> arrayInitial = new ArrayList<>();
+        List<UniqueWord> arrayFinal = new ArrayList<>();
 
 
+        //заполнение финального массива уникальными словами и количеством повторений
+        for (String word:arrayInitial){
+            if (arrayFinal.indexOf(word)==-1){
+                UniqueWord finalWord = new UniqueWord(word, Collections.frequency(arrayInitial,word));
+                arrayFinal.add(finalWord);
+            }
+        }
+        arrayInitial = null; //чистим память
+
+        //вывод значений финального листа - можно еще чистку сделать
+        for (UniqueWord word:arrayFinal){
+            System.out.printf("Word %s has frequency %d/n", word.getWord(),word.getCount());
+        }
     }
 
 }
---
+++ src/Lesson3HW/UniqueWord.java
@@ -0,0 +1,19 @@
+package Lesson3HW;
+
+public class UniqueWord {
+    private String word;
+    private int count;
+
+    public UniqueWord (String word, int count){
+        this.word = word;
+        this.count = count;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
