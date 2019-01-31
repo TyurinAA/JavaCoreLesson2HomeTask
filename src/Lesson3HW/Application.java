95d0aaac3d246da4f2fe34b41d3ad350936166a7
Andrey.Tyurin <aatyurin@outlook.com>
2019-01-31 10:52:38 +0300

    new array list created

---
+++ src/Lesson3HW/Application.java
@@ -0,0 +1,13 @@
+package Lesson3HW;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<String> array = new ArrayList<>();
+
+
+    }
+
+}
