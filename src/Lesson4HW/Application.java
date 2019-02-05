3de9ebbda14f236dba12581d84fa3f0fbb5aabfe
Andrey.Tyurin <aatyurin@outlook.com>
2019-02-05 15:58:43 +0300

    Home Work done

--- src/Lesson4HW/Application.java
+++ src/Lesson4HW/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("New homework");
         new MainWindow();
+        //new Window1();
     }
 }
--- src/Lesson4HW/MainWindow.java
+++ src/Lesson4HW/MainWindow.java
@@ -2,12 +2,40 @@ package Lesson4HW;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
+    private JButton button;
+    private JTextField textField;
+    private JList<String> list;
+    private DefaultListModel <String> model;
+
     public MainWindow() throws HeadlessException {
-    setTitle("New net Chart");
-    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    setBounds(200,200,500,500);
-    setVisible(true);
+
+        setTitle("New net Chart");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(200,200,500,500);
+        textField = new JTextField();
+        button = new JButton("Button");
+        model = new DefaultListModel<String>();
+        list = new JList<String>(model);
+        add(textField,BorderLayout.NORTH);
+        add(button, BorderLayout.SOUTH);
+        add(list, BorderLayout.CENTER);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                model.add(model.size(),textField.getText());
+            }
+        });
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                model.add(model.size(),textField.getText());
+            }
+        });
+        setVisible(true);
+
     }
 }
