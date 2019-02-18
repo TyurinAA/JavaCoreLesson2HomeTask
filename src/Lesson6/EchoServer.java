c2e13ac0fda29aff201f8a103d495c2e2875088c
Andrey.Tyurin <aatyurin@outlook.com>
2019-02-18 12:40:17 +0300

    Messages Mass Send from server added - HW6 done

--- src/Lesson6/EchoServer.java
+++ src/Lesson6/EchoServer.java
@@ -1,8 +1,6 @@
 package Lesson6;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,38 +10,19 @@ public class EchoServer {
         try (ServerSocket serverSocket = new ServerSocket(7777)) {
             while (true) {
                 Socket socket = serverSocket.accept();
-
                 ConnectionThread connectionThread = new ConnectionThread(socket);
                 System.out.println("Connection started");
                 connectionThread.start();
-                /*
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("thread created ");
-                        try (DataInputStream inp = new DataInputStream(socket.getInputStream());
-                             DataOutputStream out = new DataOutputStream((socket.getOutputStream()))){
-
-                            while (true){
-                                String msg = inp.readUTF();
-                                System.out.println("Message: " + msg);
-                                out.writeUTF(msg);
-                                out.flush();
-                            }
-
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-                }).start();*/
+                MsgMassSendThread msgMassSendThread= new MsgMassSendThread(socket);
+                msgMassSendThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
     public static class ConnectionTread extends Thread{
         @Override
         public void run() {
@@ -61,22 +40,58 @@ public class EchoServer {
         @Override
         public void run() {
             System.out.println("thread created ");
+                MsgCaptureThread msgCaptureThread = new MsgCaptureThread(socket);
+                msgCaptureThread.start();
+        }
+    }
+
+    public static class MsgCaptureThread extends Thread{
+        private Socket socket;
+
+        public MsgCaptureThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
             try (DataInputStream inp = new DataInputStream(socket.getInputStream());
                  DataOutputStream out = new DataOutputStream((socket.getOutputStream()))){
-
-                while (true){
-                    String msg = inp.readUTF();
-                    System.out.println("Message: " + msg);
-                    out.writeUTF(msg);
-                    out.flush();
-                }
-
-
+                    while (true) {
+                        String msg = inp.readUTF();
+                        System.out.println("Message: " + msg);
+                        out.writeUTF(msg);
+                        out.flush();
+                    }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
+    public static class MsgMassSendThread extends Thread{
+        private Socket socket;
+
+        public MsgMassSendThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (DataOutputStream out = new DataOutputStream((socket.getOutputStream()));
+                 BufferedReader reader = new BufferedReader( new InputStreamReader(System.in))){
+                while (true) {
+                    String massMsg = reader.readLine();
+                    System.out.println("Message for mass sent: " + massMsg);
+                    out.writeUTF(massMsg);
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 
 }
